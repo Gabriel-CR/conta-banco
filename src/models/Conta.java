ad014fc864ff7e21466204d80584d983658fb64d
Gabriel Castro <cg.castrorodrigues@gmail.com>
2023-10-01 11:21:49 -0300

    :lipstick: feat: make Conta model

---
+++ src/models/Conta.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Conta {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public Conta(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+}
